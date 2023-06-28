8b4161b624256ef80cecd62538836feeb45e5ae3
Ryan Weaver <ryan@thatsquality.com>
2023-06-28 13:15:34 -0400

    Adding functional test for AssetMapper integration

--- tests/TailwindBuilderTest.php
+++ tests/TailwindBuilderTest.php
@@ -11,21 +11,36 @@ namespace Symfonycasts\TailwindBundle\Tests;
 
 use PHPUnit\Framework\TestCase;
 use Symfony\Component\Filesystem\Filesystem;
+use Symfony\Component\Finder\Finder;
 use Symfonycasts\TailwindBundle\TailwindBuilder;
 
 class TailwindBuilderTest extends TestCase
 {
-    public function testIntegration(): void
+    public function setUp(): void
     {
         $fs = new Filesystem();
         if (file_exists(__DIR__.'/fixtures/var/tailwind')) {
             $fs->remove(__DIR__.'/fixtures/var/tailwind');
         }
         $fs->mkdir(__DIR__.'/fixtures/var/tailwind');
+    }
+
+    public function tearDown(): void
+    {
+        $finder = new Finder();
+        $finder->in(__DIR__.'/fixtures/var/tailwind')->files();
+        foreach ($finder as $file) {
+            unlink($file->getRealPath());
+        }
+    }
+
+    public function testIntegration(): void
+    {
+
 
         $builder = new TailwindBuilder(
             __DIR__.'/fixtures',
-            __DIR__.'/fixtures/app.css',
+            __DIR__.'/fixtures/assets/styles/app.css',
             __DIR__.'/fixtures/var/tailwind'
         );
         $process = $builder->runBuild(false);
