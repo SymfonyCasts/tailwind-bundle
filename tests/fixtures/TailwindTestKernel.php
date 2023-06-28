7c0b6148cdec374a2394a89afef9d9ed80e4cd4d
Ryan Weaver <ryan@thatsquality.com>
2023-06-28 13:15:34 -0400

    Adding functional test for AssetMapper integration

---
+++ tests/fixtures/TailwindTestKernel.php
@@ -0,0 +1,68 @@
+<?php
+
+/*
+ * This file is part of the SymfonyCasts TailwindBundle package.
+ * Copyright (c) SymfonyCasts <https://symfonycasts.com/>
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Symfonycasts\TailwindBundle\Tests\fixtures;
+
+use Symfony\Bundle\FrameworkBundle\FrameworkBundle;
+use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
+use Symfony\Component\Config\Loader\LoaderInterface;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\HttpKernel\Kernel;
+use Symfonycasts\TailwindBundle\SymfonycastsTailwindBundle;
+
+class TailwindTestKernel extends Kernel
+{
+    use MicroKernelTrait;
+
+    public function __construct()
+    {
+        parent::__construct('test', true);
+    }
+
+    public function registerBundles(): array
+    {
+        return [
+            new FrameworkBundle(),
+            new SymfonycastsTailwindBundle(),
+        ];
+    }
+
+    protected function configureContainer(ContainerBuilder $container, LoaderInterface $loader): void
+    {
+        $container->loadFromExtension('framework', [
+            'secret' => 'foo',
+            'test' => true,
+            'http_method_override' => true,
+            'asset_mapper' => [
+                'paths' => [
+                    __DIR__.'/assets',
+                ],
+            ],
+        ]);
+
+        $container->loadFromExtension('symfonycasts_tailwind', [
+            'input_css' => __DIR__.'/assets/styles/app.css',
+        ]);
+    }
+
+    public function getCacheDir(): string
+    {
+        return sys_get_temp_dir().'/cache'.spl_object_hash($this);
+    }
+
+    public function getLogDir(): string
+    {
+        return sys_get_temp_dir().'/logs'.spl_object_hash($this);
+    }
+
+    public function getProjectDir(): string
+    {
+        return __DIR__;
+    }
+}
