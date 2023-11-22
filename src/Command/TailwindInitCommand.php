51d5a7c7a0e991c42746b52cb2c1cbebf51028ef
Mrtn Schndlr <mrtnschndlr@gmail.com>
2023-11-22 15:33:25 +0100

    Update TailwindInitCommand.php

--- src/Command/TailwindInitCommand.php
+++ src/Command/TailwindInitCommand.php
@@ -24,7 +24,6 @@ class TailwindInitCommand extends Command
 {
     public function __construct(
         private TailwindBuilder $tailwindBuilder,
-        private string $projectDir,
     ) {
         parent::__construct();
     }
