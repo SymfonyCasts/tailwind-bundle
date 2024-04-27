73ae14dce9b3a5276508deded630dedfd301f816
Torabarnob203311003 <118876454+maxarnob@users.noreply.github.com>
2024-04-28 04:11:48 +0600

    #52

--- src/TailwindBinary.php
+++ src/TailwindBinary.php
@@ -4,7 +4,7 @@
  * This file is part of the SymfonyCasts TailwindBundle package.
  * Copyright (c) SymfonyCasts <https://symfonycasts.com/>
  * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
+ * file that was distributed with this source code....
  */
 
 namespace Symfonycasts\TailwindBundle;
