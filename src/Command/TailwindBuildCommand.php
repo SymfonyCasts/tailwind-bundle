dee9535a2aceb0c172692abb19e9db5fa1af70fa
Raphael Alogou <raphalogou@gmail.com>
2024-06-03 04:10:33 +0100

    feat: Provide input css file as an optional argument instead of an option to `tailwind:build` command

--- src/Command/TailwindBuildCommand.php
+++ src/Command/TailwindBuildCommand.php
@@ -11,6 +11,7 @@ namespace Symfonycasts\TailwindBundle\Command;
 
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
@@ -32,7 +33,7 @@ class TailwindBuildCommand extends Command
     protected function configure(): void
     {
         $this
-            ->addOption('input', 'i', InputOption::VALUE_OPTIONAL, 'The input CSS file')
+            ->addArgument('input', InputArgument::OPTIONAL, 'The input CSS file to compile')
             ->addOption('watch', 'w', null, 'Watch for changes and rebuild automatically')
             ->addOption('poll', null, null, 'Use polling instead of filesystem events when watching')
             ->addOption('minify', 'm', InputOption::VALUE_NONE, 'Minify the output CSS')
@@ -48,7 +49,7 @@ class TailwindBuildCommand extends Command
             watch: $input->getOption('watch'),
             poll: $input->getOption('poll'),
             minify: $input->getOption('minify'),
-            inputFile: $input->getOption('input'),
+            inputFile: $input->getArgument('input'),
         );
         $process->wait(function ($type, $buffer) use ($io) {
             $io->write($buffer);
